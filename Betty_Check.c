3ab199d3bdb12d06b140457f62a17e7248dc64b8
BakaneN <mizbkn@gmail.com>
2024-08-25 09:48:48 -0700

    task 0

---
+++ Betty_Check.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - function that prints alphabets
+ * Return: 0
+ */
+
+int main(void)
+{
+	char alp;
+
+	for (alp = 'a'; alp <= 'z'; alp++)
+	{
+		printf("%c ", alp"\n");
+	}
+
+return (0);
+}
